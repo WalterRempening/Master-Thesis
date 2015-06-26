d97e09b50376df27f6fbd8be6651cb0b2c1a790c
Walter Rempening Diaz <w.rempeningd@gmail.com>
2015-06-26 12:02:39 +0200

    Delete PDF and Web References

--- Web References/unu - embrace urban life_files/unu-forms.js
+++
@@ -1,9 +0,0 @@
-/**
- * @module unu-forms.js
- * This module enables custom look for form elements.
- */
-+function ($) {
-    $(document).ready(function () {
-        $('select').unuselect();
-    });
-}(jQuery);
--- Web References/unu - embrace urban life_files/validation(1).js
+++
@@ -1,9 +0,0 @@
-Validation.add('validate-select','Please select an option.', function(sValue, $Element){
-    if ($Element.refreshSelectBoxIt) {
-        $Element.refreshSelectBoxIt();
-    }
-    if (Validation.get('IsEmpty').test(sValue)) {
-        return false;
-    }
-    return true;
-});
